4c71671c7aeae50da244bd45d82a067a1db5afc4
Zoltus <zoltus@outlook.com>
2022-10-24 21:21:41 +0300

    Fixed gamemode cmd removed useless lang

---
+++ src/io/github/zoltus/onecore/player/command/arguments/GamemodeArgument.java
@@ -0,0 +1,56 @@
+package io.github.zoltus.onecore.player.command.arguments;
+
+import dev.jorel.commandapi.arguments.ArgumentSuggestions;
+import dev.jorel.commandapi.arguments.CustomArgument;
+import dev.jorel.commandapi.arguments.StringArgument;
+import io.github.zoltus.onecore.data.configuration.yamls.Commands;
+import io.github.zoltus.onecore.data.configuration.yamls.Lang;
+import io.github.zoltus.onecore.player.command.OneArgument;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+public class GamemodeArgument extends CustomArgument<GameMode, String> implements OneArgument {
+
+    public GamemodeArgument() {
+        super(new StringArgument(Lang.NODES_GAMEMODE.getString()), (info) -> {
+            GameMode gm = getGamemode(info.input());
+            if (gm == null) {
+                throw new CustomArgument.CustomArgumentException(Lang.GAMEMODE_INVALID_GAMEMODE.getString());
+            } else {
+                return gm;
+            }
+        });
+        replaceSuggestions(ArgumentSuggestions
+                .strings((info) -> toSuggestion(info.currentArg(), Commands.GAMEMODE_SUGGESTIONS.getAsArray())));
+    }
+
+    /**
+     * @param input gamemode string
+     * @return Gamemode based on aliases in config
+     */
+    private static GameMode getGamemode(String input) {
+        Bukkit.getConsoleSender().sendMessage("aaaaa");
+        Predicate<Commands> hasGm = (gamemodes) -> Arrays
+                .stream(gamemodes.getAsArray()).anyMatch(input::equalsIgnoreCase);
+        Bukkit.getConsoleSender().sendMessage("bbbbbb");
+        if (hasGm.test(Commands.GAMEMODE_ALIASES_SURVIVAL)) {
+            Bukkit.getConsoleSender().sendMessage("ccccc");
+            return GameMode.SURVIVAL;
+        } else if (hasGm.test(Commands.GAMEMODE_ALIASES_CREATIVE)) {
+            Bukkit.getConsoleSender().sendMessage("ddddd");
+            return GameMode.CREATIVE;
+        } else if (hasGm.test(Commands.GAMEMODE_ALIASES_ADVENTURE)) {
+            Bukkit.getConsoleSender().sendMessage("eeee");
+            return GameMode.ADVENTURE;
+        } else if (hasGm.test(Commands.GAMEMODE_ALIASES_SPECTATOR)) {
+            Bukkit.getConsoleSender().sendMessage("ffff");
+            return GameMode.SPECTATOR;
+        } else {
+            Bukkit.getConsoleSender().sendMessage("gggg");
+            return null;
+        }
+    }
+}
--- src/io/github/zoltus/onecore/player/command/commands/admin/Gamemode.java
+++ src/io/github/zoltus/onecore/player/command/commands/admin/Gamemode.java
@@ -1,63 +1,49 @@
 package io.github.zoltus.onecore.player.command.commands.admin;
 
 import dev.jorel.commandapi.ArgumentTree;
-import dev.jorel.commandapi.arguments.Argument;
-import dev.jorel.commandapi.arguments.ArgumentSuggestions;
-import dev.jorel.commandapi.arguments.CustomArgument;
-import dev.jorel.commandapi.arguments.StringArgument;
-import io.github.zoltus.onecore.data.configuration.IConfig;
 import io.github.zoltus.onecore.data.configuration.yamls.Commands;
-import io.github.zoltus.onecore.data.configuration.yamls.Config;
 import io.github.zoltus.onecore.data.configuration.yamls.Lang;
+import io.github.zoltus.onecore.player.command.Command;
 import io.github.zoltus.onecore.player.command.IOneCommand;
+import io.github.zoltus.onecore.player.command.arguments.GamemodeArgument;
 import io.github.zoltus.onecore.player.command.arguments.OfflinePlayerArgument;
+import io.github.zoltus.onecore.player.nbt.NBTPlayer;
 import org.bukkit.GameMode;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import io.github.zoltus.onecore.player.command.Command;
-import io.github.zoltus.onecore.player.nbt.NBTPlayer;
 
 import java.util.Arrays;
 import java.util.function.Predicate;
 
+import static io.github.zoltus.onecore.data.configuration.IConfig.MODE_PH;
+import static io.github.zoltus.onecore.data.configuration.IConfig.PLAYER_PH;
+import static io.github.zoltus.onecore.data.configuration.yamls.Commands.*;
 import static org.bukkit.GameMode.SURVIVAL;
 
 public class Gamemode implements IOneCommand {
 
-    private Argument<?> gamemodeArgument() {
-        return new CustomArgument<>(new StringArgument(Lang.NODES_GAMEMODE.getString()), (info) -> {
-            GameMode gm = getGamemode(info.input());
-            if (gm == null) {
-                throw new CustomArgument.CustomArgumentException(Lang.GAMEMODE_INVALID_GAMEMODE.getString());
-            } else if (!info.sender().hasPermission(Commands.GAMEMODE_MODE_PERMISSION.asPermission() + gm.name().toLowerCase())) {
-                throw new CustomArgument.CustomArgumentException(Lang.GAMEMODE_MODE_PERMISSION_MISSING.rp(IConfig.MODE_PH, Config.PERMISSION_PREFIX.asPermission() + gm.name().toLowerCase()));
-            } else {
-                return gm;
-            }
-        }).replaceSuggestions(ArgumentSuggestions
-                .strings((info) -> toSuggestion(info.currentArg(), Commands.GAMEMODE_SUGGESTIONS.getAsArray())));
-    }
-
     @Override
     public void init() {
         //gamemode creative
-        ArgumentTree arg0 = gamemodeArgument()
+        ArgumentTree arg0 = new GamemodeArgument()
                 .executesPlayer((player, args) -> {
                     GameMode gm = (GameMode) args[0];
                     String gmName = getGmName(gm);
                     player.setGameMode(gm);
-                    player.sendMessage(Lang.GAMEMODE_CHANGED.rp(IConfig.MODE_PH, gmName));
+                    player.sendMessage(Lang.GAMEMODE_CHANGED.rp(MODE_PH, gmName));
                 });
         //gamemode creative <player>
         ArgumentTree arg1 = new OfflinePlayerArgument()
                 .executes((sender, args) -> {
                     handleTarget(sender, (OfflinePlayer) args[1], (GameMode) args[0]);
                 });
-        new Command(Commands.GAMEMODE_LABEL)
-                .withPermission(Commands.GAMEMODE_PERMISSION)
-                .withAliases(Commands.GAMEMODE_ALIASES)
-                .then(arg0.then(arg1)).override();
+        //gamemode
+        new Command(GAMEMODE_LABEL)
+                .withPermission(GAMEMODE_PERMISSION)
+                .withAliases(GAMEMODE_ALIASES)
+                .then(arg0.then(arg1))
+                .override();
     }
 
     /**
@@ -71,7 +57,7 @@ public class Gamemode implements IOneCommand {
         String gmName = getGmName(gm);
         if (target != null && target == sender) {
             target.setGameMode(gm);
-            target.sendMessage(Lang.GAMEMODE_CHANGED.rp(IConfig.MODE_PH, gmName));
+            target.sendMessage(Lang.GAMEMODE_CHANGED.rp(MODE_PH, gmName));
         } else {
             boolean gmChanged;
             //Change gamemodes
@@ -89,10 +75,10 @@ public class Gamemode implements IOneCommand {
             }
             if (gmChanged) {
                 sender.sendMessage(Lang.GAMEMODE_TARGETS_GAMEMODE_CHANGED
-                        .rp(IConfig.MODE_PH, gmName, IConfig.PLAYER_PH, offTarget.getName()));
+                        .rp(MODE_PH, gmName, PLAYER_PH, offTarget.getName()));
             } else {
                 sender.sendMessage(Lang.GAMEMODE_TARGET_ALREADY_IN_GAMEMODE
-                        .rp(IConfig.MODE_PH, gmName, IConfig.PLAYER_PH, offTarget.getName()));
+                        .rp(MODE_PH, gmName, PLAYER_PH, offTarget.getName()));
             }
         }
     }
