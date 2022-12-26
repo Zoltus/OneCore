d09d139fc4f48d8137b9282f0cfcd509f6c86385
Zoltus <noatuiskula@gmail.com>
2022-12-26 22:15:29 +0200

    updated cmdapi

--- src/io/github/zoltus/onecore/OneCore.java
+++ src/io/github/zoltus/onecore/OneCore.java
@@ -40,7 +40,7 @@ import java.util.stream.Stream;
         @Library("org.bstats:bstats-bukkit:3.0.0"),
         @Library("org.apache.commons:commons-compress:1.21"),
         @Library("org.apache.logging.log4j:log4j-core:2.18.0"),
-        @Library("dev.jorel:commandapi-shade:8.5.1")
+        @Library("dev.jorel:commandapi-shade:8.7.1")
 })
 @Getter
 public final class OneCore extends JavaPlugin implements Listener {
