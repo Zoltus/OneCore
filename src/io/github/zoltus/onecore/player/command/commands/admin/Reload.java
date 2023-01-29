1fd3bf489d716284b1f1cf091d9587883c6f04d9
Zoltus <zoltus@outlook.com>
2023-01-29 11:40:36 +0200

    removed unused lang, renamed to moticore

--- src/io/github/zoltus/onecore/player/command/commands/admin/Reload.java
+++ src/io/github/zoltus/onecore/player/command/commands/admin/Reload.java
@@ -3,26 +3,29 @@ package io.github.zoltus.onecore.player.command.commands.admin;
 import io.github.zoltus.onecore.data.configuration.Yamls;
 import io.github.zoltus.onecore.player.command.Command;
 import io.github.zoltus.onecore.player.command.ICommand;
+import org.bukkit.Bukkit;
 
 import java.util.stream.Stream;
 
 import static io.github.zoltus.onecore.data.configuration.yamls.Commands.*;
-import static io.github.zoltus.onecore.data.configuration.yamls.Lang.RELOAD_RELOADED;
+import static io.github.zoltus.onecore.data.configuration.yamls.Lang.*;
 
 public class Reload implements ICommand {
 
     @Override
     public void init() {
-        //onecore reload
-        new Command(plugin.getName())
+        //onecore reload todo onecore reload all/config
+        new Command("onecore")
                 .withAliases("oc")
                 .then(multiLiteralArgument(RELOAD_LABEL, RELOAD_ALIASES)
                         .withPermission(RELOAD_PERMISSION.asPermission())
                         .executes((sender, args) -> {
-                            //Reloads all ymls, async had some issues
-                            Stream.of(Yamls.values()).forEach(yaml -> yaml.getYml().reload());
-                            //Sets all data to enums
-                            RELOAD_RELOADED.send(sender);
+                            Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+                                //Reloads all ymls
+                                Stream.of(Yamls.values()).forEach(yaml -> yaml.getYml().reload());
+                                //Sets all data to enums
+                                RELOAD_RELOADED.send(sender);
+                            });
                         }))
                 .override();
     }
