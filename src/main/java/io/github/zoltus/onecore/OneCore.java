39522d59408e634672a7e3b62d0f7bb714c5050e
Zoltus <noatuiskula@gmail.com>
2023-01-29 23:14:52 +0200

    Removed unit testing, Mockbukkit isn't even close to ready for testing bigger projects.

--- src/main/java/io/github/zoltus/onecore/OneCore.java
+++ src/main/java/io/github/zoltus/onecore/OneCore.java
@@ -1,5 +1,6 @@
 package io.github.zoltus.onecore;
 
+import de.tr7zw.changeme.nbtapi.NBTContainer;
 import dev.jorel.commandapi.CommandAPI;
 import dev.jorel.commandapi.CommandAPIBukkitConfig;
 import io.github.zoltus.onecore.data.BackupHandler;
@@ -17,10 +18,8 @@ import net.milkbowl.vault.economy.Economy;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoadOrder;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.java.JavaPluginLoader;
 import org.bukkit.plugin.java.annotation.dependency.Libraries;
 import org.bukkit.plugin.java.annotation.dependency.Library;
 import org.bukkit.plugin.java.annotation.dependency.SoftDependency;
@@ -49,36 +48,33 @@ import java.util.stream.Stream;
         @Library("org.bstats:bstats-bukkit:3.0.0"),
         @Library("org.apache.commons:commons-compress:1.21"),
         @Library("org.apache.logging.log4j:log4j-core:2.18.0"),
-        @Library("dev.jorel:commandapi-shade:8.7.1")
+       // @Library("dev.jorel:commandapi-shade:9.0.0-SNAPSHOT") Coming when snapshot is released
 })
 @Getter
 public final class OneCore extends JavaPlugin implements Listener {
     @Getter
     private static OneCore plugin;
-
     private Economy vault;
     private Database database;
     private BackupHandler backupHandler;
     private CommandHandler commandHandler;
 
-    //Unit testing
-    public OneCore() {
-        super();
-    }
-
-    //Unit testing
-    protected OneCore(JavaPluginLoader loader, PluginDescriptionFile description, File dataFolder, File file) {
-        super(loader, description, dataFolder, file);
-    }
-
     @Override
     public void onLoad() {
         plugin = this;
         //Loads commandapi
-        CommandAPI.onLoad(new CommandAPIBukkitConfig(this).verboseOutput(false));
+        // Load the CommandAPI
+        CommandAPI.onLoad(
+                // Configure the CommandAPI
+                new CommandAPIBukkitConfig(this)
+                        // Turn on verbose output for command registration logs
+                        .verboseOutput(false)
+                        // Give file where Brigadier's command registration tree should be dumped
+                        .dispatcherFile(new File(getDataFolder(), "command_registration.json"))
+                        // Point to the NBT API we want to use
+                        .initializeNBTAPI(NBTContainer.class, NBTContainer::new));
     }
 
-    //todo backup interval to config
     @Override
     public void onEnable() {
         //Loads commandapi
