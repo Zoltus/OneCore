1f9459e56e0ab0df3904a6bd5074be7323740b8a
Zoltus <zoltus@outlook.com>
2023-02-04 12:45:57 +0200

    Added sound back to mentions

--- src/main/java/io/github/zoltus/onecore/data/configuration/yamls/Lang.java
+++ src/main/java/io/github/zoltus/onecore/data/configuration/yamls/Lang.java
@@ -62,6 +62,7 @@ public enum Lang implements IConfig {
     KILLALL_REMOVED_ENTITYS("killall.removed-entitys"),
     MENTION_COLOR("other.mention-color"),
     MENTION_PERMISSION("other.mention-permission"),
+    MENTION_SOUND("other.mention-sound"),
     MSG_RECEIVED_MSG("msg.received-msg"),
     MSG_SENT_MSG("msg.sent-msg"),
     NODES_AMOUNT("other.nodes.amount"),
