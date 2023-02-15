07adee75ae8fdf2095c88ecbbaebcebe39b47ae1
Zoltus <zoltus@outlook.com>
2023-02-15 15:24:26 +0200

    Improved UserArgument. Now there wont be conflicts if datfile doesnt exist and user info does.

--- src/main/java/io/github/zoltus/onecore/player/command/arguments/UserArgument.java
+++ src/main/java/io/github/zoltus/onecore/player/command/arguments/UserArgument.java
@@ -6,6 +6,7 @@ import dev.jorel.commandapi.arguments.StringArgument;
 import io.github.zoltus.onecore.player.User;
 import io.github.zoltus.onecore.player.command.IArgument;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 
 import static io.github.zoltus.onecore.data.configuration.yamls.Lang.NODES_PLAYER;
 import static io.github.zoltus.onecore.data.configuration.yamls.Lang.PLAYER_NEVER_VISITED_SERVER;
@@ -19,9 +20,10 @@ public class UserArgument extends CustomArgument<User, String> implements IArgum
     public UserArgument(String add) {
         super(new StringArgument(NODES_PLAYER.getString() + add), info -> {
             String input = info.input();
-            User user = User.of(Bukkit.getOfflinePlayer(input));
-            if (user == null) {
-                throw new CustomArgument.CustomArgumentException(PLAYER_NEVER_VISITED_SERVER.getString());
+            OfflinePlayer offP = Bukkit.getOfflinePlayer(input);
+            User user = User.of(offP);
+            if (!offP.hasPlayedBefore() || user == null) {
+                throw new CustomArgumentException(PLAYER_NEVER_VISITED_SERVER.getString());
             } else {
                 return user;
             }
