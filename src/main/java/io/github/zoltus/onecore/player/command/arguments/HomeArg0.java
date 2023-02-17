93a060fff35672b0fdae211fe385d619ded097e4
Zoltus <zoltus@outlook.com>
2023-02-17 23:30:06 +0200

    Removed player suggestions from admins

--- src/main/java/io/github/zoltus/onecore/player/command/arguments/HomeArg0.java
+++ src/main/java/io/github/zoltus/onecore/player/command/arguments/HomeArg0.java
@@ -2,13 +2,11 @@ package io.github.zoltus.onecore.player.command.arguments;
 
 import dev.jorel.commandapi.arguments.ArgumentSuggestions;
 import dev.jorel.commandapi.arguments.StringArgument;
+import io.github.zoltus.onecore.player.User;
 import io.github.zoltus.onecore.player.command.IArgument;
-import org.apache.commons.lang.ArrayUtils;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import io.github.zoltus.onecore.player.User;
 
-import static io.github.zoltus.onecore.data.configuration.yamls.Commands.HOME_PERMISSION_OTHER;
 import static io.github.zoltus.onecore.data.configuration.yamls.Lang.NODES_HOME_NAME_OR_PLAYER;
 
 public class HomeArg0 extends StringArgument implements IArgument {
@@ -19,12 +17,7 @@ public class HomeArg0 extends StringArgument implements IArgument {
         replaceSuggestions(ArgumentSuggestions.strings(info -> {
             CommandSender sender = info.sender();
             User target = User.of((Player) sender);
-            String[] homes = target.getHomeArray();
-            if (!sender.hasPermission(HOME_PERMISSION_OTHER.asPermission())) {
-                return homes;
-            } else {
-                return (String[]) ArrayUtils.addAll(homes, playerSuggestions(info.currentArg()));
-            }
+            return target.getHomeArray();
         }));
     }
 }
