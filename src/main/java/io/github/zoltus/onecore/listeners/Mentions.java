c5491117206103f8c83d11e36683bbb7aa70800f
Zoltus <zoltus@outlook.com>
2023-02-22 13:46:01 +0200

    Fixed config. Mentions show now perplayer

--- src/main/java/io/github/zoltus/onecore/listeners/Mentions.java
+++ src/main/java/io/github/zoltus/onecore/listeners/Mentions.java
@@ -1,6 +1,6 @@
 package io.github.zoltus.onecore.listeners;
 
-import io.github.zoltus.onecore.data.configuration.yamls.Lang;
+import io.github.zoltus.onecore.data.configuration.yamls.Config;
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -14,29 +14,32 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static io.github.zoltus.onecore.data.configuration.yamls.Lang.MENTION_COLOR;
-
-
 public class Mentions implements Listener {
-
-    private final String MENTION_COLORS = MENTION_COLOR.getString();
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void chatMention2(AsyncPlayerChatEvent e) {
-        String orgMsg = e.getMessage();
-        Matcher matcher = Pattern.compile("@(\\w+)")
-                .matcher(e.getMessage());
+        if (!e.getPlayer().hasPermission(Config.MENTION_PERMISSION.asPermission())) {
+            return;
+        }
+        String orginal = e.getMessage();
+        Matcher matcher = Pattern.compile("@(\\w+)").matcher(orginal);
         while (matcher.find()) {
             Player target = Bukkit.getPlayer(matcher.group(1));
             int start = matcher.start();
             if (target != null /*&& !player.equals(sender)*/) {
                 e.setCancelled(true);
-                String beforeColor = ChatColor.getLastColors(orgMsg.substring(0, start));
+                String beforeColor = ChatColor.getLastColors(orginal.substring(0, start));
                 String continueColor = StringUtils.defaultIfEmpty(beforeColor, "§f");
-                String message = e.getMessage().replace(matcher.group(),
-                        MENTION_COLORS + target.getDisplayName() + continueColor);
-                e.setMessage(message);
-                target.playSound(target.getLocation(), Sound.valueOf(Lang.MENTION_SOUND.get()), 1, 1);
+                String message = orginal.replace(matcher.group(),
+                        Config.MENTION_COLOR.getString() + target.getDisplayName() + continueColor);
+                Bukkit.getOnlinePlayers().forEach(player -> {
+                    if (player.equals(target)) {
+                        player.sendMessage(message);
+                        target.playSound(target.getLocation(), Sound.valueOf(Config.MENTION_SOUND.get()), 1, 1);
+                    } else {
+                        player.sendMessage(orginal);
+                    }
+                });
             }
         }
     }
