1f702d5d1fd19a59d7384540eeaad2cf68c2b883
Zoltus <zoltus@outlook.com>
2023-03-03 12:48:29 +0200

    Added /tp <coord3D> <player>

--- src/main/java/io/github/zoltus/onecore/player/command/commands/admin/Tp.java
+++ src/main/java/io/github/zoltus/onecore/player/command/commands/admin/Tp.java
@@ -8,11 +8,11 @@ import io.github.zoltus.onecore.data.configuration.yamls.Lang;
 import io.github.zoltus.onecore.player.command.Command;
 import io.github.zoltus.onecore.player.command.ICommand;
 import io.github.zoltus.onecore.player.command.arguments.OfflinePlayerArgument;
-import io.github.zoltus.onecore.player.teleporting.LocationUtils;
+import io.github.zoltus.onecore.player.nbt.NBTPlayer;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import io.github.zoltus.onecore.player.nbt.NBTPlayer;
 
 public class Tp implements ICommand {
     @Override
@@ -24,37 +24,46 @@ public class Tp implements ICommand {
                     Location destination = getLoc(offlineTarget);
                     sender.teleport(destination);
                     if (offlineTarget.isOnline()) {
-                        Lang.TP_TELEPORTED_TARGET.send(sender, IConfig.PLAYER_PH, offlineTarget.getName());
+                        Lang.TP_TELEPORTED_TARGET.send(sender,
+                                IConfig.PLAYER_PH, offlineTarget.getName());
                     } else {
-                       Lang.TP_TELEPORTED_OFFLINE_TARGET.send(sender, IConfig.PLAYER_PH, offlineTarget.getName());
+                       Lang.TP_TELEPORTED_OFFLINE_TARGET.send(sender,
+                               IConfig.PLAYER_PH, offlineTarget.getName());
                     }
                 });
-        //todo shorten
         //tp <player> <player>
         Argument<?> arg1 = new OfflinePlayerArgument("2")
                 .executes((sender, args) -> {
                     OfflinePlayer fromOff = (OfflinePlayer) args.get(0);
                     OfflinePlayer target = (OfflinePlayer) args.get(1);
                     Location destination = getLoc(target);
-                    tp(fromOff, destination);
+                    tp(sender, fromOff, destination);
                     if (fromOff.isOnline() && target.isOnline()) {
-                        Lang.TP_TELEPORTED_TARGETS.send(sender, IConfig.PLAYER_PH, fromOff.getName(), IConfig.PLAYER2_PH, target.getName());
+                        Lang.TP_TELEPORTED_TARGETS.send(sender,
+                                IConfig.PLAYER_PH, fromOff.getName(),
+                                IConfig.PLAYER2_PH, target.getName());
                     } else {
-                        Lang.TP_TELEPORTED_OFFLINE_TARGETS.send(sender, IConfig.PLAYER_PH, fromOff.getName(), IConfig.PLAYER2_PH, target.getName());
+                        Lang.TP_TELEPORTED_OFFLINE_TARGETS.send(sender,
+                                IConfig.PLAYER_PH, fromOff.getName(),
+                                IConfig.PLAYER2_PH, target.getName());
                     }
                 });
+
+        //tp <coord> <player>
+        Argument<?> locArg2 = new OfflinePlayerArgument("2")
+                .withPermission("")
+                .executes((sender, args) -> {
+                    Location destination = (Location) args.get(0);
+                    OfflinePlayer offTarget = (OfflinePlayer) args.get(1);
+                    tp(sender, offTarget, destination);
+                });
+
         //tp <coord>
         Argument<?> locArg = new LocationArgument("loc")
                 .executesPlayer((sender, args) -> {
                     Location destination = (Location) args.get(0);
-                    tp(sender, destination);
-                });
-        /*//tp <coord> <player>
-        Argument<?> locArg2 = new OfflinePlayerArgument("2")
-                .withPermission("")
-                .executes((sender, args) -> {
-
-                });*/
+                    tp(sender, sender, destination);
+                }).then(locArg2);
         //tp
         new Command(Commands.TP_LABEL)
                 //todo fix perm
@@ -70,11 +79,13 @@ public class Tp implements ICommand {
                 .then(new OfflinePlayerArgument()
                         .executesPlayer((sender, args) -> {
                             OfflinePlayer offlineTarget = (OfflinePlayer) args.get(0);
-                            tp(offlineTarget, sender.getLocation());
+                            tp(sender, offlineTarget, sender.getLocation());
                             if (offlineTarget.isOnline()) {
-                                Lang.TPHERE_TELEPORTED.send(sender, IConfig.PLAYER_PH, offlineTarget.getName());
+                                Lang.TPHERE_TELEPORTED.send(sender,
+                                        IConfig.PLAYER_PH, offlineTarget.getName());
                             } else {
-                                Lang.TPHERE_OFFLINE_TARGET.send(sender, IConfig.PLAYER_PH, offlineTarget.getName());
+                                Lang.TPHERE_OFFLINE_TARGET.send(sender,
+                                        IConfig.PLAYER_PH, offlineTarget.getName());
                             }
                         })).override();
     }
@@ -89,10 +100,10 @@ public class Tp implements ICommand {
         }
     }
 
-    private void tp(OfflinePlayer offTarget, Location loc) {
+    private void tp(CommandSender sender, OfflinePlayer offTarget, Location loc) {
         Player target = offTarget.getPlayer();
         if (target != null) {
-            LocationUtils.teleportSafeAsync(target, loc);
+            target.teleport(loc);
         } else {
             NBTPlayer nbtPlayer = new NBTPlayer(offTarget);
             nbtPlayer.setLocation(loc);
