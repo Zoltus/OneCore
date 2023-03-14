017cb416f356b538bb997856c0f9a3f764a5708b
Zoltus <zoltus@outlook.com>
2023-03-14 12:29:43 +0200

    Fixed error when player teleported and left the server.

--- src/main/java/io/github/zoltus/onecore/player/teleporting/DelayedTeleport.java
+++ src/main/java/io/github/zoltus/onecore/player/teleporting/DelayedTeleport.java
@@ -19,6 +19,7 @@ public class DelayedTeleport {
 
     private Location loc;
     private final User user;
+    //Target to teleport to, if null it will teleport to loc
     private User target;
     private final BukkitTask teleTask;
 
@@ -37,11 +38,15 @@ public class DelayedTeleport {
     private BukkitTask start() {
         TP_STARTED.send(user, SECONDS_PH, DELAY);
         return Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            //If target is set and isnt online
             if (target != null && !target.isOnline()) {
                 TP_TARGET_QUIT.replace(PLAYER_PH, target.getName());
             } else {
                 Location destination = target == null ? loc : target.getPlayer().getLocation();
-                LocationUtils.teleportSafeAsync(user.getPlayer(), destination);
+                //Checks if teleporter is still online.
+                if (user.isOnline()) {
+                    LocationUtils.teleportSafeAsync(user.getPlayer(), destination);
+                }
             }
             user.setTeleport(null);
         }, 20L * DELAY);
