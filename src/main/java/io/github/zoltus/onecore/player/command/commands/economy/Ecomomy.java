ce276f42524b3d159b761966f76b69c3f9cc20af
Zoltus <zoltus@outlook.com>
2023-04-23 17:29:21 +0300

    Renamed eco cmd

--- src/main/java/io/github/zoltus/onecore/player/command/commands/economy/Ecomomy.java
+++ src/main/java/io/github/zoltus/onecore/player/command/commands/economy/Ecomomy.java
@@ -16,7 +16,7 @@ import java.util.*;
 import static io.github.zoltus.onecore.data.configuration.yamls.Commands.*;
 import static io.github.zoltus.onecore.data.configuration.yamls.Lang.*;
 
-public class EconomyCMD implements ICommand {
+public class Ecomomy implements ICommand {
 
     @Override
     public void init() {
