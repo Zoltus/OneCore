4b15d7e05cc2d6a2bfe1ebb890a0e2060850fbc7
Zoltus <zoltus@outlook.com>
2023-06-12 09:58:17 +0300

    Small optimizations on command suggestions

--- src/main/java/io/github/zoltus/onecore/player/command/IArgument.java
+++ src/main/java/io/github/zoltus/onecore/player/command/IArgument.java
@@ -4,7 +4,9 @@ import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public interface IArgument {
 
@@ -13,13 +15,21 @@ public interface IArgument {
     }
 
     default String[] playerSuggestions(String input) {
-        return filter(input, Bukkit.getOnlinePlayers().stream()
-                .map(Player::getName).toArray(String[]::new));
+        List<String> list = new ArrayList<>();
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            String name = player.getName();
+            list.add(name);
+        }
+        return filter(input, list.toArray(new String[0]));
     }
 
     default String[] filter(String input, String... suggestions) {
-        return Arrays.stream(suggestions)
-                .filter(word -> StringUtils.startsWithIgnoreCase(word, input))
-                .toArray(String[]::new);
+        List<String> list = new ArrayList<>();
+        for (String word : suggestions) {
+            if (StringUtils.startsWithIgnoreCase(word, input)) {
+                list.add(word);
+            }
+        }
+        return list.toArray(new String[0]);
     }
 }
