fcd720180ab2e77ecee9cd5864ce835db1b96df5
Zoltus <zoltus@outlook.com>
2023-06-14 19:24:29 +0300

    jenkins test commit.

--- src/main/java/io/github/zoltus/onecore/OneCore.java
+++ src/main/java/io/github/zoltus/onecore/OneCore.java
@@ -25,7 +25,7 @@ import java.util.List;
 
 @Getter
 public final class OneCore extends JavaPlugin {
-
+//    jenkins test commit.
     @Getter
     private static OneCore plugin;
     private Economy vault;
