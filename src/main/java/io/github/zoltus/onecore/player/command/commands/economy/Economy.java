dfa400e3b0ae217b0c1fa35b050c74f5537abb53
Zoltus <zoltus@outlook.com>
2023-06-30 15:38:59 +0300

    Fixed typo

--- src/main/java/io/github/zoltus/onecore/player/command/commands/economy/Economy.java
+++ src/main/java/io/github/zoltus/onecore/player/command/commands/economy/Economy.java
@@ -16,7 +16,7 @@ import java.util.*;
 import static io.github.zoltus.onecore.data.configuration.yamls.Commands.*;
 import static io.github.zoltus.onecore.data.configuration.yamls.Lang.*;
 
-public class Ecomomy implements ICommand {
+public class Economy implements ICommand {
 
     @Override
     public void init() {
