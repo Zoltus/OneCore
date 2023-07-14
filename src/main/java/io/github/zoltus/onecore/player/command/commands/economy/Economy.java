b1df0aae554f41c8a1f2868aa9d378b3cc6f05bb
Zoltus <zoltus@outlook.com>
2023-07-14 17:22:28 +0300

    Removed some some economy alias stuff

--- src/main/java/io/github/zoltus/onecore/player/command/commands/economy/Economy.java
+++ src/main/java/io/github/zoltus/onecore/player/command/commands/economy/Economy.java
@@ -13,6 +13,10 @@ import org.bukkit.command.CommandSender;
 
 import java.util.*;
 
+import static io.github.zoltus.onecore.data.configuration.yamls.Commands.AMOUNT_PH;
+import static io.github.zoltus.onecore.data.configuration.yamls.Commands.BALANCE_PH;
+import static io.github.zoltus.onecore.data.configuration.yamls.Commands.PLAYER2_PH;
+import static io.github.zoltus.onecore.data.configuration.yamls.Commands.PLAYER_PH;
 import static io.github.zoltus.onecore.data.configuration.yamls.Commands.*;
 import static io.github.zoltus.onecore.data.configuration.yamls.Lang.*;
 
@@ -130,11 +134,7 @@ public class Economy implements ICommand {
         //Singles
         balance.register();
         balanceOther.register();
-        give.override();
-        transfer.override();
-        take.override();
         pay.override();
-        set.override();
         baltop.override();
         baltopPage.register();
 
