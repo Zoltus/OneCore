9c14fe659475828cc153e74da34a05a40edf1a88
Zoltus <zoltus@outlook.com>
2023-07-14 18:35:27 +0300

    Improved playerargument completion

--- src/main/java/io/github/zoltus/onecore/player/command/arguments/PlayerArgument.java
+++ src/main/java/io/github/zoltus/onecore/player/command/arguments/PlayerArgument.java
@@ -1,15 +1,20 @@
 package io.github.zoltus.onecore.player.command.arguments;
 
+import dev.jorel.commandapi.arguments.ArgumentSuggestions;
+import io.github.zoltus.onecore.player.command.IArgument;
+
 import static io.github.zoltus.onecore.data.configuration.yamls.Lang.NODES_PLAYER;
 
-public class PlayerArgument extends dev.jorel.commandapi.arguments.PlayerArgument {
+public class PlayerArgument extends dev.jorel.commandapi.arguments.PlayerArgument implements IArgument {
 
     //Used so I wouldnt have to repeat node name.
     public PlayerArgument() {
         this("");
     }
-    //todo remove @everyone from this arg switch to entityselector
+
     public PlayerArgument(String add) {
         super(NODES_PLAYER.getString() + add);
+        replaceSuggestions(ArgumentSuggestions
+                .strings(info -> playerSuggestions(info.currentArg())));
     }
 }
