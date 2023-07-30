bd654158c313816f555f539f91c0bf679c4833fe
Zoltus <zoltus@outlook.com>
2023-07-30 22:26:56 +0300

    added todo

--- src/main/java/io/github/zoltus/onecore/player/command/arguments/HomeArg0.java
+++ src/main/java/io/github/zoltus/onecore/player/command/arguments/HomeArg0.java
@@ -16,6 +16,7 @@ public class HomeArg0 extends StringArgument implements IArgument {
 
     //try to register cmd as /home <player> and /home <string>
     public HomeArg0() {
+        //todo check which arg this is based on current input/prev args and return according to it
         super(NODES_HOME_NAME_OR_PLAYER.getString());
         replaceSuggestions(ArgumentSuggestions.strings(info -> {
             CommandSender sender = info.sender();
