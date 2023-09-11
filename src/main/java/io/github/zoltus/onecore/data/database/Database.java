795c9db349c118d1aa1734e4c53c79f7abde2b2a
Zoltus <zoltus@outlook.com>
2023-09-11 21:34:28 +0300

    Database rework progress

--- src/main/java/io/github/zoltus/onecore/data/database/Database.java
+++ src/main/java/io/github/zoltus/onecore/data/database/Database.java
@@ -1,10 +1,7 @@
 package io.github.zoltus.onecore.data.database;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import io.github.zoltus.onecore.OneCore;
 import io.github.zoltus.onecore.data.configuration.yamls.Config;
-import io.github.zoltus.onecore.economy.OneEconomy;
 import io.github.zoltus.onecore.player.User;
 import io.github.zoltus.onecore.player.teleporting.PreLocation;
 import org.bukkit.Bukkit;
@@ -13,7 +10,6 @@ import org.bukkit.scheduler.BukkitScheduler;
 import org.sqlite.SQLiteConfig;
 
 import java.sql.*;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -27,7 +23,7 @@ public class Database {
     private Database(OneCore plugin) {
         plugin.getLogger().info("Loading database...");
         this.plugin = plugin;
-        this.connection = connection(); //test
+        this.connection = connection();
         //todo if these are normal settings anyways
         // "PRAGMA mmap_size = 30000000000;
         this.config.setJournalMode(SQLiteConfig.JournalMode.WAL);
@@ -50,11 +46,10 @@ public class Database {
                     || connection.isClosed() ? DriverManager.getConnection(url, config.toProperties()) : connection;
         } catch (Exception e) {
             throw new DatabaseException("§4Database connection failed!\n §c" + e.getMessage());
-        }                // Balance
+        }
     }
 
     private void createTables() {
-        // Creates tables
         try (Connection con = connection(); Statement stmt = con.createStatement()) {
             stmt.execute("""
                     CREATE TABLE IF NOT EXISTS players(
@@ -89,23 +84,6 @@ public class Database {
         }
     }
 
-    /*
-    private void addColumnIfNotExists(Statement stmt, String tableName, String columnName, String columnDefinition) throws SQLException {
-        ResultSet rs = stmt.executeQuery("PRAGMA table_info(" + tableName + ");");
-        boolean columnExists = false;
-        while (rs.next()) {
-            String existingColumnName = rs.getString("name");
-            if (existingColumnName.equals(columnName)) {
-                columnExists = true;
-                break;
-            }
-        }
-
-        if (!columnExists) {
-            stmt.execute("ALTER TABLE " + tableName + " ADD COLUMN " + columnName + " " + columnDefinition + ";");
-        }
-    }*/
-
     private void initAutoSaver() {
         scheduler.runTaskTimerAsynchronously(plugin, this::saveUsersAsync,
                 (saveInterval * 20L) * 60, (saveInterval * 20L) * 60);
@@ -120,59 +98,50 @@ public class Database {
      */
     public void saveData() {
         final String sqlPlayers = "INSERT INTO players (uuid, tpenabled, isvanished) VALUES (?, ?, ?)";
-        //todo save balance from balances map if use oneeconomy
         final String sqlHomes = "INSERT INTO homes (uuid, name, world, x, y, z, yaw, pitch) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
         final String sqlBalances = "INSERT INTO balances (uuid, balance) VALUES (?, ?)";
-
         try (Connection con = connection()
-             ; PreparedStatement pStmPlayers = con.prepareStatement(sqlPlayers)
-             ; PreparedStatement pStmHomes = con.prepareStatement(sqlHomes)
-             ; PreparedStatement pStmBalances = con.prepareStatement(sqlBalances)
-        ) {
-            //todo balances load separate
-
-            // Save balances
-            if (Config.ECONOMY_USE_ONEECONOMY.getBoolean()) {
-                for (Map.Entry<UUID, Double> entry : OneEconomy.getBalances().entrySet()) {
-                    Double bal = entry.getValue();
-                    String uuid = entry.getKey().toString();
-                    pStmBalances.setString(1, uuid);
-                    pStmBalances.setDouble(2, bal);
-                    pStmBalances.addBatch();
-                }
-            }
-            //Save user homes and data
+             ; PreparedStatement playerStmt = con.prepareStatement(sqlPlayers)
+             ; PreparedStatement homeStmt = con.prepareStatement(sqlHomes)
+             ; PreparedStatement balanceStmt = con.prepareStatement(sqlBalances)) {
+            //Save user homeStmt and data
             for (Map.Entry<UUID, User> entry : User.getUsers().entrySet()) {
                 User user = entry.getValue();
-                UUID uuid = user.getUniqueId();
+                String uuid = user.getUniqueId().toString();
+                // Saves economy if OneEconomy is enabled
+                if (Config.ECONOMY_USE_ONEECONOMY.getBoolean()) {
+                    balanceStmt.setString(1, uuid);
+                    balanceStmt.setDouble(2, user.getBalance());
+                    balanceStmt.addBatch();
+                }
                 // Player data
-                pStmPlayers.setString(1, uuid.toString());
-                pStmPlayers.setBoolean(2, user.isTpEnabled());
-                pStmPlayers.setBoolean(3, user.isVanished());
-                pStmPlayers.executeUpdate();
+                playerStmt.setString(1, uuid);
+                playerStmt.setBoolean(2, user.isTpEnabled());
+                playerStmt.setBoolean(3, user.isVanished());
+                playerStmt.addBatch();
                 // Homes
                 for (Map.Entry<String, PreLocation> homeEntry : user.getHomes().entrySet()) {
                     PreLocation home = homeEntry.getValue();
-                    pStmHomes.setString(1, uuid.toString());
-                    pStmHomes.setString(2, homeEntry.getKey());
-                    pStmHomes.setString(3, home.getWorldName());
-                    pStmHomes.setDouble(4, home.getX());
-                    pStmHomes.setDouble(5, home.getY());
-                    pStmHomes.setDouble(6, home.getZ());
-                    pStmHomes.setDouble(7, home.getYaw());
-                    pStmHomes.setDouble(8, home.getPitch());
-                    pStmHomes.addBatch();
+                    homeStmt.setString(1, user.getUniqueId().toString());
+                    homeStmt.setString(2, homeEntry.getKey());
+                    homeStmt.setString(3, home.getWorldName());
+                    homeStmt.setDouble(4, home.getX());
+                    homeStmt.setDouble(5, home.getY());
+                    homeStmt.setDouble(6, home.getZ());
+                    homeStmt.setDouble(7, home.getYaw());
+                    homeStmt.setDouble(8, home.getPitch());
+                    homeStmt.addBatch();
                 }
-                //Add Batch
-                pStmBalances.executeBatch();
-                pStmPlayers.addBatch();
-                pStmHomes.executeBatch();
+                playerStmt.addBatch();
             }
-            pStmPlayers.executeBatch();
+            homeStmt.executeBatch();
+            balanceStmt.executeBatch();
+            playerStmt.executeBatch();
         } catch (SQLException e) {
             throw new DatabaseException("§4Error saving players!\n §c" + e.getMessage());
         }
     }
+
 
     public void cacheData() {
         long l = System.currentTimeMillis();
@@ -180,16 +149,27 @@ public class Database {
         String sql = """
                 SELECT players.uuid, tpenabled, isvanished, balances.balance
                     FROM players LEFT JOIN balances ON players.uuid = balances.uuid;""";
+
+        String sqlHomes = "SELECT * from homes;";
+
         try (Connection con = connection()
              ; PreparedStatement pStm = con.prepareStatement(sql)
+             ; PreparedStatement pStm = con.prepareStatement(sqlHomes)
+             //todo homes
              ; ResultSet rs = pStm.executeQuery()) {
             int index = 0;
             while (rs.next()) {
                 String uuid = rs.getString("uuid");
                 OfflinePlayer offP = Bukkit.getOfflinePlayer(UUID.fromString(uuid));
-                if (User.of(offP) == null) { //If user havent been loaded yet by login it loads it
-                    userFromResult(offP, rs); // without this loading could happen twice
-                }
+                User newUser = new User(offP);
+
+                //String homes = rs.getString("homes");
+                double balance = rs.getDouble("balance");
+                boolean isvanished = rs.getBoolean("isvanished");
+                boolean tpenabled = rs.getBoolean("tpenabled");
+                newUser.setVanished(isvanished);
+                newUser.setBalance(balance);
+                newUser.setTpEnabled(tpenabled);
                 index++;
             }
             plugin.getLogger().info("Finished caching " + index + " users, took: " + (System.currentTimeMillis() - l) + "ms");
@@ -198,21 +178,6 @@ public class Database {
         }
     }
 
-    private void userFromResult(OfflinePlayer offP, ResultSet rs) throws SQLException {
-        boolean isvanished = rs.getBoolean("isvanished");
-        boolean tpenabled = rs.getBoolean("tpenabled");
-        String homes = rs.getString("homes");
-        double balance = rs.getDouble("balance");
-        User newUser = new User(offP);
-        newUser.setVanished(isvanished);
-        newUser.setBalance(balance);
-        newUser.setTpEnabled(tpenabled);
-        Gson gson = new Gson();
-        if (homes != null) {
-            newUser.setHomes(gson.fromJson(homes, new TypeToken<HashMap<String, PreLocation>>() {
-            }.getType()));
-        }
-    }
 
     public static class DatabaseException extends RuntimeException {
         public DatabaseException(String message) {
