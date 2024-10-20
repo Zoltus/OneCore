47ef7ac592bd38f33fb864e0d1836c80c18957d5
Zoltus <zoltus@outlook.com>
2024-10-20 17:24:29 +0300

    send ping <player> msg now to sender and not target

--- src/main/java/io/github/zoltus/onecore/player/command/commands/regular/Ping.java
+++ src/main/java/io/github/zoltus/onecore/player/command/commands/regular/Ping.java
@@ -20,7 +20,7 @@ public class Ping implements ICommand {
                 .withPermission(Commands.PING_OTHER_PERMISSION.asPermission())
                 .executes((sender, args) -> {
                     Player target = (Player) args.get(0);
-                    PING_TARGETS_PING.send(target, PING_PH, target.getPing(), PLAYER_PH, target.getName());
+                    PING_TARGETS_PING.send(sender, PING_PH, target.getPing(), PLAYER_PH, target.getName());
                 });
         //Ping
         new Command(PING_LABEL)
