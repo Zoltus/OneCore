71197160b8415e55b9fcc2f905b193bcead88346
Zoltus <zoltus@outlook.com>
2025-04-11 20:59:49 +0300

    add proper minimessage support for broadcast

--- src/main/java/io/github/zoltus/onecore/player/command/commands/admin/Broadcast.java
+++ src/main/java/io/github/zoltus/onecore/player/command/commands/admin/Broadcast.java
@@ -1,14 +1,14 @@
 package io.github.zoltus.onecore.player.command.commands.admin;
 
 import dev.jorel.commandapi.arguments.ChatArgument;
+import io.github.zoltus.onecore.data.configuration.LangBuilder;
 import io.github.zoltus.onecore.data.configuration.yamls.Commands;
 import io.github.zoltus.onecore.data.configuration.yamls.Lang;
 import io.github.zoltus.onecore.player.command.Command;
 import io.github.zoltus.onecore.player.command.ICommand;
 import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 public class Broadcast implements ICommand {
 
@@ -19,11 +19,10 @@ public class Broadcast implements ICommand {
                 .withAliases(Commands.BROADCAST_ALIASES)
                 .then(new ChatArgument(Lang.NODES_MESSAGE.get())
                         .executes((sender, args) -> {
-                            BaseComponent[] prefix = TextComponent.fromLegacyText(Lang.BROADCAST_PREFIX.get());
-                            BaseComponent[] message = (BaseComponent[]) args.get(0);
-                            ComponentBuilder builder = new ComponentBuilder();
-                            builder.append(prefix).append(message);
-                            Bukkit.spigot().broadcast(builder.create());
+                            BaseComponent[] components = (BaseComponent[]) args.get(0);
+                            String message = Lang.BROADCAST_PREFIX.get() + BaseComponent.toLegacyText(components);
+                            LangBuilder langBuilder = new LangBuilder(message);
+                            langBuilder.send(Bukkit.getOnlinePlayers().toArray(Player[]::new));
                         })
                 ).override();
     }
